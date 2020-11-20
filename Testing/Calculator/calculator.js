0c9dd9e319cf9ec9f32a8719e91a6a3e26015eed
Joy Christine <joychristin2@gmail.com>
2020-11-20 13:35:03 +0300

    test passed

--- Testing/Calculator/calculator.js
+++ Testing/Calculator/calculator.js
@@ -1,10 +1,3 @@
-// const calculator = {
-//     add: (num1, num2) => num1 + num2,
-//     subtract: (num1, num2) => num1 - num2,
-//     multiply: (num1, num2) => num1 * num2,
-//     divide: (num1, num2) => num1 / num2,
-// };
-// module.exports = calculator;
 const calculator = {
     add: (a, b) => a + b,
     subtract: (a, b) => a - b,
