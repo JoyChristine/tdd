0b5bbd40b26d147f9a30b057fb0f86a237998d8b
Joy Christine <joychristin2@gmail.com>
2020-11-20 13:35:27 +0300

    test passed

--- Testing/Calculator/calculator.test.js
+++ Testing/Calculator/calculator.test.js
@@ -1,17 +1,17 @@
-const calc = require("./calculator");
+const calculate = require("./calculator");
 
 test("adds", () => {
-    expect(calc.add(1, 2)).toBe(3);
+    expect(calculate.add(1, 2)).toBe(3);
 });
 
 test("subtracts", () => {
-    expect(calc.subtract(5, 2)).toBe(3);
+    expect(calculate.subtract(5, 2)).toBe(3);
 });
 
 test("multiplies", () => {
-    expect(calc.multiply(3, 3)).toBe(9);
+    expect(calculate.multiply(3, 3)).toBe(9);
 });
 
 test("divides", () => {
-    expect(calc.divide(10, 2)).toBe(5);
+    expect(calculate.divide(10, 2)).toBe(5);
 });
